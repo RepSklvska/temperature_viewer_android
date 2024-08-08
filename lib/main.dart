def390a5bcddc99ed78603bbc28d5076be9f7076
Iztecteocuitlatl Sokolov <gunpowderfans@mail.ru>
2024-08-09 00:53:07 +0800

    调整画面大小的功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:temperature_viewer/resultPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:scaled_app/scaled_app.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  ScaledWidgetsFlutterBinding.ensureInitialized();
+
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.getDouble('appScale') ?? prefs.setDouble('appScale', 1.0);
+  double scale = prefs.getDouble('appScale')!;
+
+  runAppScaled(const MyApp());
+
+  ScaledWidgetsFlutterBinding.instance.scaleFactor = (deviceSize) {
+    return scale;
+  };
 }
 
 class MyApp extends StatelessWidget {
@@ -29,6 +40,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _selectedScale = 1.0;
+
   int _queryCounter = 5;
 
   final TextEditingController _textFieldController = TextEditingController();
@@ -48,10 +61,18 @@ class _MyHomePageState extends State<MyHomePage> {
     _setTextFieldValue(queryUrl);
   }
 
+  void _initializeSliderValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _selectedScale = prefs.getDouble('appScale') ?? 1.0;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     _initializeTextField();
+    _initializeSliderValue();
   }
 
   @override
@@ -65,6 +86,26 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 32, right: 32),
+              child: Slider(
+                value: _selectedScale,
+                max: 1.5,
+                min: 0.8,
+                divisions: 7,
+                onChanged: (value) {
+                  SharedPreferences.getInstance().then((prefs) {
+                    prefs.setDouble('appScale', value);
+                  });
+                  ScaledWidgetsFlutterBinding.instance.scaleFactor = (deviceSize) {
+                    return value;
+                  };
+                  setState(() {
+                    _selectedScale = value;
+                  });
+                },
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.only(left: 32, right: 32),
               child: TextField(
